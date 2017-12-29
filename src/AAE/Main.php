df549847d3403f2a18eedccd328d3fc4981a0942
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2017-12-29 19:13:38 +0000

    Should fix.

--- src/AAE/Main.php
+++ src/AAE/Main.php
@@ -6,7 +6,7 @@ use pocketmine\item\Item;
 use pocketmine\Server;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\inventory\InventoryOpenEvent;
+use pocketmine\event\player\PlayerItemHeldEvent;
 use pocketmine\utils\TextFormat as TF;
 use pocketmine\utils\Config;
 
@@ -33,22 +33,15 @@ class Main extends PluginBase implements Listener{
 		return $this->config->get("max-level");
 	}
 
-	public function onInvOpen(InventoryOpenEvent $ev){
+	public function onItemHeld(PlayerItemHeldEvent $ev){
 		$p = $ev->getPlayer();
 		$max = $this->getMax();
 		$contents = $p->getInventory()->getContents();
-		foreach($contents as $i){
+		$i = $p->getInventory()->getItemInHand();
 			if($i instanceof Item){
 				if($i->hasEnchantments()){
 					foreach($i->getEnchantments() as $e){
-						if($e->getLevel() > $max){
+						if($e->getLevel() >= $max){
 							$p->getInventory()->removeItem($i);
-							$this->getLogger()->info("Item ".$i->getName()." has been removed from ".$p->getName()."'s inv for a enchantment level over 8!");
-							$p->sendMessage(TF::GREEN."[AntiAbusiveEnchants]".TF::BLUE.$i->getName()." has been removed from your inv for being above or eqaul to the max enchantment level!");
-						}
-					}
-				}
-			}
-		}
-	}
-}
+							$this->getServer()->getLogger()->info(TF::GREEN."[AntiAbusiveEnchants]".TF::BLUE."Item ".$i->getName()." has been removed from ".$p->getName()."'s inventory for a enchantment level over ".$this->getMax()."!");
+							$p->sendMessage(TF::GREEN."[AntiAbusiveEnchants]".TF::BLUE.$i->getName()." has been removed from your inventory for being above or equal to the max enchantment level!");
